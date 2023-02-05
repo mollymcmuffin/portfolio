0b43848e099d1607630af84a2a8f8f07e169814a
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-05 22:48:58 +0800

    added incon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-      <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@400;700&display=swap" rel="stylesheet"/>
-      <title>Zheng Xuan | Dev</title>  
-      </Head> 
+        <link rel="icon" href="/images/webicon.jpg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Zheng Xuan | Dev</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
