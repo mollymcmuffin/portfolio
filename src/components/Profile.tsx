608107ab794d8c73a181362ee592ac958db2848e
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-05 14:13:41 +0800

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='flex flex-col items-center bg-[#252A34] text-white p-5'>
+    <div className='flex flex-col items-center justify-center gap-1 bg-[#252A34] text-white p-5 h-[100vh] laptop:h-[100%]'>
         <h1 className='text-4xl'>Lee Zheng Xuan</h1>
         <h2 className='text-sm'>Year 2 Student at NTU Computer Science</h2>
 
