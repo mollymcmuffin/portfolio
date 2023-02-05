e43eb5de5980f5d7a6e2f04e1573ae0ce0ec110d
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-05 14:04:35 +0800

    sizing

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,13 +12,13 @@ const Project = ({title,desc,imglink, date}:Props) => {
   return (
     <div className='flex flex-col mb-10 bg-[#252A34] box-border gap-2'>
         <img src={imglink} alt={title}/>
-        <h2 className='text-xl'>
+        <h2 className='text-2xl'>
             {title}
         </h2>
-        <h3 className='text-slate-400 italic'>
+        <h3 className='text-slate-400 italic text-lg'>
             {date}
         </h3>
-        <p className='text-sm'>
+        <p className='text-base'>
         {desc}
         </p>
     </div>
