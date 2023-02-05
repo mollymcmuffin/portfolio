7551911700786537d1393de728e73c8f6500cafa
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-05 14:15:36 +0800

    sizing

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-1 bg-[#252A34] text-white p-5 h-[100vh] laptop:h-[100%]'>
-        <h1 className='text-4xl'>Lee Zheng Xuan</h1>
+    <div className='flex flex-col items-center justify-center gap-1 bg-[#252A34] text-white p-5 h-[100vh] laptop:h-[100%] '>
+        <h1 className='text-4xl font-bold'>Lee Zheng Xuan</h1>
         <h2 className='text-sm'>Year 2 Student at NTU Computer Science</h2>
 
     </div>
