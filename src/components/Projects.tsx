00387b661c3ba0be4543207ee7feaff25cdf9119
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-05 21:55:54 +0800

    added some text

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -51,7 +51,7 @@ const Projects = () => {
         <Project
           title={"Idle Tidle Band Page"}
           desc={
-            "A front-end web application from scratch using NextJS with TailwindCSS as styling for a friend’s band. This web app serves as an alternative to LinkTree with additional features such as integration of YouTube API to grab the latest video from the band’s channel. The web app is hosted on Vercel."
+            "Built a front-end web application from scratch using NextJS with TailwindCSS as styling for a friend’s band. This web app serves as an alternative to LinkTree with additional features such as integration of YouTube API to grab the latest video from the band’s channel. The web app is hosted on Vercel."
           }
           imglink={"/images/idletidal.png"}
           date={"June 2022 - June 2022"}
