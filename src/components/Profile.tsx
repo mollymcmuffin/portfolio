22301bac0c4bd0a2bf1acd17a8b13d62f4be7c4a
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-06 08:19:25 +0800

    added padding

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-1 bg-[#252A34] text-white p-5 h-[100vh] laptop:h-[100%] '>
-        <h1 className='text-4xl font-bold'>Lee Zheng Xuan</h1>
-        <h2 className='text-sm'>Year 2 Student at NTU Computer Science</h2>
-
+    <div className="flex flex-col items-center justify-center gap-1 bg-[#252A34] text-white p-5 h-[100vh] laptop:h-[100%] ">
+      <h1 className="font-bold text-7xl">Zheng Xuan</h1>
+      <h2 className="box-border pt-2 text-sm text-slate-400">
+        journey to fullstack
+      </h2>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
