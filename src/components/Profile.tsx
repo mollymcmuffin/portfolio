1f3bd28795c0f9c531bc9a7201a85e9985a58016
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2023-02-06 08:31:23 +0800

    changed name format

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Profile = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-1 bg-[#252A34] text-white p-5 h-[100vh] laptop:h-[100%] ">
-      <h1 className="font-bold text-7xl">Zheng Xuan</h1>
+      <div className="flex flex-wrap justify-center gap-3 ">
+        <h1 className="font-bold text-8xl">Zheng</h1>
+        <h1 className="font-bold text-8xl">Xuan</h1>
+      </div>
       <h2 className="box-border pt-2 text-sm text-slate-400">
         journey to fullstack
       </h2>
